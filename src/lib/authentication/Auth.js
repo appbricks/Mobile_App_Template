7f9f640b3aa58cf126ebd55765c60c4dee48859a
Mevan Samaratunga <mevansam@gmail.com>
2018-08-20 09:09:17 +0400

    cosmetic

--- src/lib/authentication/Auth.js
+++ src/lib/authentication/Auth.js
@@ -32,144 +32,146 @@ export function withAuth(
   C
 ) {
 
-  return class extends Component<Props> {
+  return (
+    class extends Component<Props> {
 
-    constructor(props) {
-      super(props);
+      constructor(props) {
+        super(props);
 
-      this.logger = new Logger("withAuth");
+        this.logger = new Logger("withAuth");
 
-      this.session = new Session(
-        authSession,
-        this._setReady.bind(this),
-        this._setWait.bind(this),
-        this._navigateToAuthValidationRoute.bind(this)
-      );
+        this.session = new Session(
+          authSession,
+          this._setReady.bind(this),
+          this._setWait.bind(this),
+          this._navigateToAuthValidationRoute.bind(this)
+        );
 
-      this.state = {
-        ready: false,
-        signedIn: null,
-      };
-    }
-
-    async componentDidMount() {
-
-      AppState.addEventListener('change', this._handleAppStateChange.bind(this));
-
-      // If saved session is valid then ready should be 
-      // false until session user has been re-authenticaed
-      let signedIn = await this.session.authSession.validateSession();
-      this._setReady(signedIn, !signedIn);
-
-      SplashScreen.hide();
-    }
-
-    componentWillUnmount() {
-      AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
-    }
-
-    _handleAppStateChange(nextAppState) {
-      this.logger.trace("application state change: ", nextAppState);
-
-      switch (nextAppState) {
-        case "inactive":
-          break;
-
-        case "background":
-
-          // BackgroundTimer.runBackgroundTimer(() => {
-          //   this.logger.trace("timer check at: ", Date.now());
-          // }, 3000);
-
-          if (this.state.signedIn
-            && this.session.user
-            && !this.session.user.rememberSignIn()) {
-
-            this.session.signOut();
-          } else {
-            this.session.user = null;
-          }
-          break;
-
-        case "active":
-
-          // BackgroundTimer.stopBackgroundTimer();
-
-          var validateSession = async () => {
-            this._setReady(await this.session.authSession.validateSession());
-          };
-
-          validateSession();
-          break;
-      }
-    }
-
-    _setReady(signedIn?, ready = true) {
-
-      if (typeof signedIn == "undefined") {
-
-        this.logger.trace(
-          "setting app ready state: signed in =", this.state.signedIn,
-          ", ready =", ready);
-
-        this.setState({
-          ready: ready,
-        });
-
-      } else {
-
-        this.logger.trace(
-          "setting app ready state: signed in =", signedIn,
-          ", ready =", ready);
-
-        this.setState({
-          ready: ready,
-          signedIn: signedIn
-        });
-        this.session.isSignedIn = this.state.signedIn;
-      }
-    }
-
-    _setWait(beforeWaitHandler?) {
-
-      if (beforeWaitHandler) {
-        beforeWaitHandler(this.authSession)
+        this.state = {
+          ready: false,
+          signedIn: null,
+        };
       }
 
-      this.logger.trace("setting app wait state to true")
-      this.setState({ ready: false });
+      async componentDidMount() {
+
+        AppState.addEventListener('change', this._handleAppStateChange.bind(this));
+
+        // If saved session is valid then ready should be 
+        // false until session user has been re-authenticaed
+        let signedIn = await this.session.authSession.validateSession();
+        this._setReady(signedIn, !signedIn);
+
+        SplashScreen.hide();
+      }
+
+      componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
+      }
+
+      _handleAppStateChange(nextAppState) {
+        this.logger.trace("application state change: ", nextAppState);
+
+        switch (nextAppState) {
+          case "inactive":
+            break;
+
+          case "background":
+
+            // BackgroundTimer.runBackgroundTimer(() => {
+            //   this.logger.trace("timer check at: ", Date.now());
+            // }, 3000);
+
+            if (this.state.signedIn
+              && this.session.user
+              && !this.session.user.rememberSignIn()) {
+
+              this.session.signOut();
+            } else {
+              this.session.user = null;
+            }
+            break;
+
+          case "active":
+
+            // BackgroundTimer.stopBackgroundTimer();
+
+            var validateSession = async () => {
+              this._setReady(await this.session.authSession.validateSession());
+            };
+
+            validateSession();
+            break;
+        }
+      }
+
+      _setReady(signedIn?, ready = true) {
+
+        if (typeof signedIn == "undefined") {
+
+          this.logger.trace(
+            "setting app ready state: signed in =", this.state.signedIn,
+            ", ready =", ready);
+
+          this.setState({
+            ready: ready,
+          });
+
+        } else {
+
+          this.logger.trace(
+            "setting app ready state: signed in =", signedIn,
+            ", ready =", ready);
+
+          this.setState({
+            ready: ready,
+            signedIn: signedIn
+          });
+          this.session.isSignedIn = this.state.signedIn;
+        }
+      }
+
+      _setWait(beforeWaitHandler?) {
+
+        if (beforeWaitHandler) {
+          beforeWaitHandler(this.authSession)
+        }
+
+        this.logger.trace("setting app wait state to true")
+        this.setState({ ready: false });
+      }
+
+      _navigateToAuthValidationRoute() {
+        navigateToAuthValidationRoute();
+      }
+
+      render() {
+        const { ready, signedIn } = this.state;
+
+        const {
+          session,
+          ...otherProps
+        } = this.props;
+
+        return (
+
+          <Provider
+            store={reduxStore}>
+
+            <C
+              ready={ready}
+              setReady={() => this._setReady()}
+              signedIn={signedIn}
+              session={session || this.session}
+              backgroundImage={backgroundImage}
+              {...otherProps}
+            />
+
+          </Provider>
+        );
+      }
     }
-
-    _navigateToAuthValidationRoute() {
-      navigateToAuthValidationRoute();
-    }
-
-    render() {
-      const { ready, signedIn } = this.state;
-
-      const {
-        session,
-        ...otherProps
-      } = this.props;
-
-      return (
-
-        <Provider
-          store={reduxStore}>
-
-          <C
-            ready={ready}
-            setReady={() => this._setReady()}
-            signedIn={signedIn}
-            session={session || this.session}
-            backgroundImage={backgroundImage}
-            {...otherProps}
-          />
-
-        </Provider>
-      );
-    }
-  }
+  );
 }
 
 /**
