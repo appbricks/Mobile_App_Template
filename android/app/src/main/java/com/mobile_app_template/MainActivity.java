d8f98d6917de8f9d0b848fb2638f839aca057849
Mevan Samaratunga <mevansam@gmail.com>
2018-09-03 00:08:30 +0400

    added "react-native-orientation" module

--- android/app/src/main/java/com/mobile_app_template/MainActivity.java
+++ android/app/src/main/java/com/mobile_app_template/MainActivity.java
@@ -5,11 +5,25 @@ import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
         return "Mobile_App_Template";
+    }
+
+    /**
+     * Required by "react-native-orientation" to handle orientation change events.
+     * https://github.com/yamill/react-native-orientation
+     * 
+     * @param newConfig
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        this.sendBroadcast(intent);
     }
 }
--- android/app/src/main/java/com/mobile_app_template/MainApplication.java
+++ android/app/src/main/java/com/mobile_app_template/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobile_app_template;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
             new RNAWSCognitoPackage(),
             new VectorIconsPackage(),
             new FingerprintAuthPackage(),
